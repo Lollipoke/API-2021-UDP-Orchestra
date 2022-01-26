b6ce753209908bc4106dc9c9b8e5171da38f6324
Melissa Gehring <melissagehringstx@gmail.com>
2022-01-26 09:59:11 +0100

    Add implementation

--- docker/image-auditor/src/auditor.js
+++ docker/image-auditor/src/auditor.js
@@ -18,19 +18,30 @@ const UDP_PROTOCOL_PORT = 9907;
 const TCP_PROTOCOL_PORT = 2205;
 
 /*
+ * Interval to consider a musician active
+ */
+ const INTERVAL_ACTIVE = 5000;
+
+
+/*
+ * List of instruments
+ */
+const instruments_map = new Map();
+instruments_map.set("ti-ta-ti", "piano");
+instruments_map.set("pouet", "trumpet");
+instruments_map.set("trulu", "flute");
+instruments_map.set("gzi-gzi", "violin");
+instruments_map.set("boum-boum", "drum");
+
+/*
  * List of active musicians
  */
-var musicians = [];
+var musicians = new Map();
 
 /*
  * We use a standard Node.js module to work with UDP
  */
 const dgram = require('dgram');
-
-/*
- * We use a standard Node.js module to work with TCP
- */
-const net = require('net');
 
 /* 
  * Let's create a datagram socket. We will use it to listen for datagrams published in the
@@ -46,9 +57,24 @@ s.bind(UDP_PROTOCOL_PORT, function() {
  * This call back is invoked when a new datagram has arrived.
  */
 s.on('message', function(msg, source) {
-  console.log("Data has arrived: " + msg + ". Source port: " + source.port);
+  var time = Date.now();
+  var parseMsg = JSON.parse(msg);
+  var musician = musicians.get(parseMsg.uuid);
+
+  // If musician is not in the list, add it
+  if (musician == undefined) {
+    musicians.set(parseMsg.uuid, {instrument:instruments_map.get(parseMsg.sound), start:time, last:time});
+  } else if (now - musician.last < INTERVAL_ACTIVE) {
+    // Musician has been active within 5 seconds
+    musicians.set(parseMsg.uuid, {instrument:musician.instrument, start:musician.start, last:time});
+  }
 });
 
+
+/*
+ * We use a standard Node.js module to work with TCP
+ */
+const net = require('net');
 
 /*
  * Create a TCP server that accepts connection on port 2205 and sends the list
@@ -60,3 +86,20 @@ const server = net.createServer(function(socket) {
 });
 
 server.listen(TCP_PROTOCOL_PORT, '127.0.0.1');
+
+server.on('connect', function(msg, source) {
+  var time = Date.now();
+
+  // Remove inactive musicians
+  musicians.forEach((v, k) => {
+    if(time - v.last > INTERVAL_ACTIVE) {
+      console.log("Remove musician: ", k);
+      musicians.delete(k);
+    }
+  }
+
+  var msg = JSON.stringify(musicians)
+  socket.write(msg);
+  socket.write("\n");
+  socket.end();
+});
--- docker/image-musician/src/musician.js
+++ docker/image-musician/src/musician.js
@@ -1,20 +1,28 @@
 /*
- This program simulates a "smart" thermometer, which publishes the measured temperature
- on a multicast group. Other programs can join the group and receive the measures. The
- measures are transported in json payloads with the following format:
+ This program simulates a musician, which sends the sound their instrument plays
+ on a multicast group. Other programs can join the group and receive the sounds. The
+ sounds are transported in json payloads with the following format:
 
-   {"timestamp":1394656712850,"location":"kitchen","temperature":22.5}
+   {"uuid":"aa7d8cb3-a15f-4f06-a0eb-b8feb6244a60","sound":"ti-ta-ti"}
 
- Usage: to start a thermometer, type the following command in a terminal
-        (of course, you can run several thermometers in parallel and observe that all
-        measures are transmitted via the multicast group):
+ Usage: to start a musician, launch a musician container
+        (of course, you can run several musicians in parallel and observe that all
+        sounds are transmitted via the multicast group):
 
-   node thermometer.js location temperature variation
+   docker run -d res/musician piano
 
 */
 
+/*
+ * We define the multicast address and port
+ */
 const PROTOCOL_MULTICAST_ADDRESS = "239.255.22.5";
 const PROTOCOL_PORT = 9907;
+
+/*
+ * Interval between datagrams
+ */
+ const INTERVAL_DATAGRAMS = 1000;
 
 /*
  * We use a standard Node.js module to work with UDP
@@ -27,51 +35,59 @@ var dgram = require('dgram');
 var s = dgram.createSocket('udp4');
 
 /*
- * Let's define a javascript class for our thermometer. The constructor accepts
- * a location, an initial temperature and the amplitude of temperature variation
- * at every iteration
+ * To define uuid for the musician
  */
-function Thermometer(location, temperature, variation) {
-
-	this.location = location;
-	this.temperature = temperature;
-	this.variation = variation;
+const RFC4122 = require('rfc4122');
+const uuid = new RFC4122().v4f();
 
 /*
-   * We will simulate temperature changes on a regular basis. That is something that
-   * we implement in a class method (via the prototype)
-   */
-	Thermometer.prototype.update = function() {
-		var delta = this.variation - (Math.random() * this.variation * 2);
-		this.temperature = (this.temperature + delta);
+ * List of instruments
+ */
+const instruments_map = new Map();
+instruments_map.set("piano", "ti-ta-ti");
+instruments_map.set("trumpet", "pouet");
+instruments_map.set("flute", "trulu");
+instruments_map.set("violin", "gzi-gzi");
+instruments_map.set("drum", "boum-boum");
 
 /*
-	  * Let's create the measure as a dynamic javascript object, 
-	  * add the 3 properties (timestamp, location and temperature)
-	  * and serialize the object to a JSON string
-	  */
-		var measure = {
-			timestamp: Date.now(),
-			location: this.location,
-			temperature: this.temperature
+ * Let's define a javascript class for our musician. The constructor accepts
+ * an instrument
+ */
+function Musician(instrument) {
+
+	this.instrument = instrument;
+
+  	/*
+  	 * We will simulate sound playing on a regular basis. That is something that
+  	 * we implement in a class method (via the prototype)
+  	 */
+	Musician.prototype.update = function() {
+		/*
+		 * Let's create the sound as a dynamic javascript object, 
+		 * add the 3 properties (timestamp, location and temperature)
+		 * and serialize the object to a JSON string
+		 */
+		var msg = {
+			uuid: uuid,
+			sound: instruments_map.get(instrument)
 		};
-		var payload = JSON.stringify(measure);
+		var payload = JSON.stringify(msg);
 
-/*
-	   * Finally, let's encapsulate the payload in a UDP datagram, which we publish on
-	   * the multicast address. All subscribers to this address will receive the message.
-	   */
-		message = new Buffer(payload);
-		s.send(message, 0, message.length, PROTOCOL_PORT, PROTOCOL_MULTICAST_ADDRESS, function(err, bytes) {
+	  	/*
+	  	 * Finally, let's encapsulate the payload in a UDP datagram, which we publish on
+	  	 * the multicast address. All subscribers to this address will receive the message.
+	  	 */
+		s.send(payload, PROTOCOL_PORT, PROTOCOL_MULTICAST_ADDRESS, function(err, bytes) {
 			console.log("Sending payload: " + payload + " via port " + s.address().port);
 		});
 
 	}
 
-/*
-	 * Let's take and send a measure every 500 ms
+	/*
+	 * Let's take and send a measure every seconds
 	 */
-	setInterval(this.update.bind(this), 500);
+	setInterval(this.update.bind(this), INTERVAL_DATAGRAMS);
 
 }
 
@@ -79,12 +95,19 @@ function Thermometer(location, temperature, variation) {
  * Let's get the thermometer properties from the command line attributes
  * Some error handling wouln't hurt here...
  */
-var location = process.argv[2];
-var temp = parseInt(process.argv[3]);
-var variation = parseInt(process.argv[4]);
+if(process.argv.length != 2) {
+	console.error("Missing argument");
+	process.exit();
+}
+
+var instrument = process.argv[2];
+if(instruments_map.get(instrument) == undefined) {
+	console.error("Invalid instrument");
+	process.exit();
+}
 
 /*
- * Let's create a new thermoter - the regular publication of measures will
+ * Let's create a new Musician - the regular publication of sounds will
  * be initiated within the constructor
  */
-var t1 = new Thermometer(location, temp, variation);
+var t1 = new Musician(instrument);
